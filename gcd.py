282367049e40b0dc8f76b174ceba83e086b0c221
Max <mmaslov848@gmail.com>
2025-02-20 20:25:16 +0200

    реалізовано pytest-тести

--- gcd.py
+++ gcd.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def gcd(a, b):
     while b:
         a, b = b, a % b
@@ -17,3 +20,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+def test_gcd():
+    assert gcd(48, 18) == 6
+    assert gcd(101, 103) == 1
+    assert gcd(56, 98) == 14
+    assert gcd(0, 5) == 5
+    assert gcd(7, 0) == 7
+    assert gcd(0, 0) == 0
